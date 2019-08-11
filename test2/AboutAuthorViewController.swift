b85f36e71a8e33187b494b462af1b9a422dbffe5
christinasiv <53951735+christinasiv@users.noreply.github.com>
2019-08-11 14:47:12 -0400

    Add files via upload

---
+++ test2/AboutAuthorViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AboutAuthorViewController.swift
+//  test2
+//
+//  Created by Spare on 22/07/2019.
+//  Copyright © 2019 Spare. All rights reserved.
+//
+
+import UIKit
+
+class AboutAuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func close(){
+        dismiss(animated: true, completion: nil)
+    }
+
+    
+
+}
